ca9325254da74d45ef42fab2f992d11650f20c91
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2022-09-09 20:28:49 +0530

    9th Sept 2022
    strlen function from <string.h> library in C.

---
+++ strlen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    //char *str = "Hello World";
+    //char str[] = "Hello World";
+    char str[100] = "Hello World";
+    printf("%d", strlen(str));
+    return 0;
+}
